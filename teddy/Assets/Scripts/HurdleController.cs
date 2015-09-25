eb26c0caf13c0c85640825484bf1ef2b97697df0
skatpgusskat <skatpgusskat@naver.com>
2015-09-25 17:02:26 +0900

    ini

---
+++ teddy/Assets/Scripts/HurdleController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HurdleController: MonoBehaviour {
+
+	public Flower FlowerPrefab;
+	// Use this for initialization
+	void Start () {
+		Koba_Init ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Koba_Init(){
+
+		float moveSpeed = PlatformController.moveSpeed;
+		// first Flower until 00:20:50 // 30박
+		for (float i = 3.10f; i < 30; i+= 4f) {
+			float bps = 89f / 60f;
+			float distancePerBeat = moveSpeed / bps;
+			float x = i * distancePerBeat;
+			Instantiate (FlowerPrefab, new Vector3(x, 0, 0), Quaternion.identity);
+		}
+
+		/*for (float i = 1 / (89 / 60) * 3; i < 20.5; i += 1 / (89 / 60) * 4) {
+			float x = i * 3.357215f;
+			Instantiate (FlowerPrefab, new Vector3(x, 0, 0), Quaternion.identity);
+		}*/
+
+		// bpm = beats per minute
+		// bps = bpm / 60
+		// 1초동안 나오는 비트 갯수 = bpm / 60 = bps
+		// 1초가 되기 위한 비트 갯수 = 1/bps = 1 / (bpm / 60)
+		// 89->95
+
+		// moveSpeed 5일때 89bpm에서 1박자마다 3.357215를 감.
+		// 3.357215 / moveSpeed = 1초가 되기 위한 비트 갯수  = 1/bps
+		// 1박자에 가는 길이 = moveSpeed / bps
+
+		// 1/3.357215 박자마다 1를 감.
+		// 1초동안 bpm/60개의 박자가 나옴. 그 박자마다 * 3.357215를 감.
+
+
+
+	}
+}
---
+++ teddy/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformController : MonoBehaviour {
+
+	[HideInInspector] public bool jump;
+
+	public static float moveSpeed = 5f;
+	public static float jumpSpeedInitial = 20f;
+	public static float g = 90;
+	public Transform groundCheck;
+
+	private bool isGrounded = false;
+	private Animator anim;
+	private Rigidbody2D rb2d;
+	private float currentYVelocity = 0;
+
+	public Ground GroundPrefab;
+	private Ground[] groundList;
+	private const int GroundCount = 35;
+	private int frontGroundIndex = 0;
+	private const int MaximumfrontGroundDistance = 17;
+
+	private const float Timestep = 0.02f;
+	private int count;
+	// Use this for initialization
+
+	void Awake(){
+		anim = GetComponent<Animator> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+		jump = false;
+		groundList = new Ground[GroundCount];
+
+		for (int i = 0; i < GroundCount; i++) {
+			GenerateGround (i);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		isGrounded = transform.position.y <= 0;
+		if (Input.GetButtonDown ("Jump") && isGrounded) {
+			jump = true;
+			currentYVelocity = 0;
+			isGrounded = false;
+		}
+
+		RollGround ();
+	}
+
+	void FixedUpdate()
+	{
+		anim.SetFloat ("Speed", 1);
+
+		if (jump) {
+			anim.SetTrigger("Jump");
+			currentYVelocity = jumpSpeedInitial;
+			jump = false;
+		}
+
+		if (isGrounded == false) {
+			currentYVelocity += -g * Timestep;
+		}
+
+		if (transform.position.y + currentYVelocity * Timestep <= 0) {
+			isGrounded = true;
+			transform.position += new Vector3 ( moveSpeed * Timestep, -transform.position.y, 0);
+			currentYVelocity = 0;
+		} else {
+			transform.position += new Vector3 (moveSpeed * Timestep, currentYVelocity * Timestep, 0);
+		}
+		/*
+		count++;
+		if (count == 674) {
+			count = 0;
+			Debug.Log (transform.position.x);
+		}*/
+	}
+
+	void GenerateGround(int index)
+	{
+		groundList[index] = (Ground)Instantiate (GroundPrefab, new Vector3(index, -1, 0), Quaternion.identity);
+	}
+
+	void RollGround()
+	{
+		if (groundList [frontGroundIndex].transform.position.x + MaximumfrontGroundDistance < transform.position.x) {
+			groundList [frontGroundIndex].transform.position += new Vector3 (GroundCount, 0, 0);
+			++frontGroundIndex;
+			if(frontGroundIndex >= GroundCount)
+			{
+				frontGroundIndex = 0;
+			}
+			RollGround ();
+		}
+	}
+}
